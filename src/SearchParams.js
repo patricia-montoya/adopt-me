85f88fc7ff1c848ecd2d4c05040ce995345ddf13
PMontoya <pmontoya@lean-tech.io>
2020-02-13 11:49:11 -0500

    Adding another component, getting data

---
+++ src/SearchParams.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  // const [breeds, setBreeds] = useState([]);
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        {/* <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map(breedStr => (
+              <option key={breedStr} value={breedStr}>
+                {breedStr}
+              </option>
+            ))}
+          </select>
+        </label> */}
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
